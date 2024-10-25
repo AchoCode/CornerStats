36dae0b7db5991718e8c8a3ab59275f1f4b3c46b
achoCode <luckyacho2@gmail.com>
2024-10-26 00:27:40 +0100

    Backend and Frontend communication success, api integration next

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
 app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'ASDFGHJKL;WERTYUIOP'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        print(data)
     return jsonify({'Message':'connection successful'})
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
   const [apiData, setApiData] = useState({});
+  const [homeTeam, setHomeTeam] = useState();
+  const [awayTeam, setAwayTeam] = useState();
+  const [formData, setFormData] = useState({});
   const apiUrl = "http://127.0.0.1:5000";
+
   let response;
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -18,10 +23,57 @@ function App() {
     };
     fetchData();
   }, []);
+
+  useEffect(() => {
+    const postForm = () => {
+      setFormData((preFormData) => ({
+        ...preFormData,
+        homeTeam,
+        awayTeam,
+      }));
+    };
+
+    postForm();
+  }, [homeTeam, awayTeam]);
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let response = await axios.post(apiUrl, formData)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <div>
-        {apiData ? apiData.Message : "Loading..."} {/* Render the data */}
+      <div className="page-container">
+        <div className="match-wrapper">
+          <form onSubmit={handleOnSubmit}>
+            <div className="input-box">
+              <input
+                type="text"
+                onChange={(e) => {
+                  setHomeTeam(e.target.value);
+                }}
+              />
+              <label htmlFor="">Home Team</label>
+            </div>
+            <div className="input-box">
+              <input
+                type="text"
+                onChange={(e) => {
+                  setAwayTeam(e.target.value);
+                }}
+              />
+              <label htmlFor="">Away Team</label>
+            </div>
+            <button type="submit" className="btn">
+              Get details
+            </button>
+          </form>
+          {apiData ? apiData.Message : "Loading..."} {/* Render the data */}
+        </div>
       </div>
     </>
   );
